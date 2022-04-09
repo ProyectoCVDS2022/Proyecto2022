7a76bc56492be8a83a6c8dba6273a04523ee921d
unknown <david.leon-m@labinfo.is.escuelaing.edu.co>
2022-04-09 09:51:37 -0500

    inicio base de datos

---
+++ Proyecto CVDS/tablas.sql
@@ -0,0 +1,40 @@
+--Drops
+drop table "Proyecto".usuarios;
+drop table "Proyecto".recursos;
+drop table "Proyecto".tipoRecurso;
+drop table "Proyecto".ubicacionRecursos;
+drop table "Proyecto".reservas;
+--Crear tablas
+CREATE TABLE  "Proyecto".usuarios(
+	id INT not null,
+	rol VARCHAR(30) not null,
+	correo VARCHAR(30) not null,
+	clave VARCHAR(30) not null
+);
+
+CREATE TABLE "Proyecto".recursos (
+	id int NOT NULL,
+	tipo int NOT NULL,
+	fechaIncio DATE not null,
+	fechaFin DATE not null,
+	ubicacion int NOT NULL,
+	observaciones varchar(30) NOT NULL
+);
+create table "Proyecto".tipoRecursos(
+	id int NOT NULL,
+	nombre varchar(30) NOT NULL,
+	descripcion varchar(30)
+);
+create table "Proyecto".ubicacionRecursos(
+	id int NOT NULL,
+	lugar varchar(30) NOT NULL
+);
+create table "Proyecto".reservas(
+	usuario INT not null,
+	recurso int NOT NULL,
+	fechaIncio DATE not null,
+	fechaFin DATE not null
+);
+--Primarias
+--Forareas
+--Unicas
