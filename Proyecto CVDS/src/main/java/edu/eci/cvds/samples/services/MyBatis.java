77994eb9039ef6ee58e2cf8d9f1861f322b5c868
Santiago Bayona <santiago.bayona@mail.escuelaing.edu.co>
2022-04-18 20:27:48 -0500

    A

---
+++ Proyecto CVDS/src/main/java/edu/eci/cvds/samples/services/MyBatis.java
@@ -0,0 +1,47 @@
+package edu.eci.cvds.samples.services;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+
+public class MyBatis {
+
+    /**
+     * Método que construye una fábrica de sesiones de MyBatis a partir del
+     * archivo de configuración ubicado en src/main/resources
+     *
+     * @return instancia de SQLSessionFactory
+     */
+    public static SqlSessionFactory getSqlSessionFactory() {
+        SqlSessionFactory sqlSessionFactory = null;
+        if (sqlSessionFactory == null) {
+            InputStream inputStream;
+            try {
+                inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    /**
+     * Programa principal de ejempo de uso de MyBATIS
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(String args[]) throws SQLException, ParseException {
+        SqlSessionFactory sessionfact = getSqlSessionFactory();
+        SqlSession sqlss = sessionfact.openSession();
+        /*IniciativaMapper i=sqlss.getMapper(IniciativaMapper.class);*/
+        sqlss.commit();
+        sqlss.close();
+    }
+}
